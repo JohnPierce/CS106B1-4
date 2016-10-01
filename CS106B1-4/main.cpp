79a9c2eaef1f36f2a4b4a83249440e1181f49e6a
John Pierce <jjvppierce@sbcglobal.net>
2016-09-30 22:20:16 -0700

    Initial Commit

---
+++ CS106B1-4/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CS106B1-4
+//
+//  Created by John Pierce on 9/30/16.
+//  Copyright © 2016 JohnPierce. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
